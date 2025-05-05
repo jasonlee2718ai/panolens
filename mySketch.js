dd5a29068a37e33d997b6453ce3549dd2f4473e0
jasonlee2718ai <jasonlee2718.ai@gmail.com>
2025-05-06 01:28:40 +0800

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -3,13 +3,37 @@ let viewer;
 
 viewer = new PANOLENS.Viewer({
   output:'console', 
-  autoHideInfospot:false, 
-  cameraFov:'80', 
+  autoHideInfospot:true, 
+  cameraFov:'90', 
+  autoRotate: false,
   controlBar: true,
-  enableReticle: true
+  controlButtons: ['fullscreen', 'setting', 'video']
 });
+  
+let pano_name = "M3_Drone_Shot_equirectangular-jpg_beautiful_colorful_aurora_borealis_2063885359_12236741.jpg";
+let pano_url = "https://images.blockadelabs.com/images/imagine/"+pano_name;
 
-//panorama = new PANOLENS.ImagePanorama( 'M3_Watercolor_equirectangular-jpg_a_big_studio_with_1264537915_13711509.jpg' );
-panorama = new PANOLENS.ImagePanorama( 'https://images.blockadelabs.com/images/imagine/M3_Watercolor_equirectangular-jpg_a_big_studio_with_1264537915_13711509.jpg?ver=1' );
+panorama = new PANOLENS.ImagePanorama( pano_name );  
+//panorama = new PANOLENS.ImagePanorama( pano_url );
 viewer.add( panorama );
 viewer.setPanorama(panorama);
+
+function setGyro() {
+  if (typeof DeviceOrientationEvent !== 'function') {
+    return console.log('DeviceOrientationEvent not detected')
+  }
+  if (typeof DeviceOrientationEvent.requestPermission !== 'function') {
+    return console.log('DeviceOrientationEvent.requestPermission not detected')
+  }
+  DeviceOrientationEvent.requestPermission().then(function(result) {
+    $("button").hide();
+		
+  });
+}
+
+
+
+
+
+
+
