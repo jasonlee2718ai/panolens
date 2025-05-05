458f2ae224f10b59b38753733d1ccd232d198e01
jasonlee2718ai <jasonlee2718.ai@gmail.com>
2025-05-06 01:23:01 +0800

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -9,7 +9,7 @@ viewer = new PANOLENS.Viewer({
   enableReticle: true
 });
 
-panorama = new PANOLENS.ImagePanorama( 'M3_Watercolor_equirectangular-jpg_a_big_studio_with_1264537915_13711509.jpg' );
-//panorama = new PANOLENS.ImagePanorama( 'https://images.blockadelabs.com/images/imagine/Radiant_Realism_equirectangular-jpg_A_professional_engineers_studio_1185582729_10639200.jpg?ver=1' );
+//panorama = new PANOLENS.ImagePanorama( 'M3_Watercolor_equirectangular-jpg_a_big_studio_with_1264537915_13711509.jpg' );
+panorama = new PANOLENS.ImagePanorama( 'https://images.blockadelabs.com/images/imagine/M3_Watercolor_equirectangular-jpg_a_big_studio_with_1264537915_13711509.jpg?ver=1' );
 viewer.add( panorama );
 viewer.setPanorama(panorama);
